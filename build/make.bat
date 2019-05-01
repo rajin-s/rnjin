da29dfecee1dbd850fc00bca0771f056a284f757
Rajin <rajshank>
2019-05-01 14:50:36 -0400

    Added run command to make script

--- build/make.bat
+++ build/make.bat
@@ -1,24 +1,30 @@
 @echo off
 
-SET output_log = scons.log
+SET output_log=scons.log
 
 if "%1%"=="" (goto make_default)
 if %1% == clean (goto make_clean) else (goto make_arg)
 
 echo "something went wrong :("
-goto end
+goto _exit
 
 :make_clean
-    scons make=all -c > %output_log
-    goto end
+    cmd /c scons make=all -c > %output_log%
+    goto _end
 
 :make_arg
-    scons make=%1 > %output_log
-    goto end
+    cmd /c scons make=%1 > %output_log%
+    goto _end
 
 :make_default
-    scons make=all > %output_log
-    goto end
+    cmd /c scons make=all > %output_log%
+    goto _end
 
-:end
-    echo "done!"
+:_end
+    if "%2%"=="run" (goto run) else (goto _exit)
+
+:run
+    cmd /c .\rnjin.exe
+    goto _exit
+
+:_exit
