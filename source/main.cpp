ef08d0543e8f77dcae4f05fe628473ee900459ac
Rajin <rajshank>
2019-05-01 14:56:50 -0400

    Changed main to test wrapper struct

--- source/main.cpp
+++ source/main.cpp
@@ -8,13 +8,53 @@
 
 #include "core.hpp"
 #include "log.hpp"
+#include "script.hpp"
+
+template <int V>
+void print_num()
+{
+    log::main << "Instruction " << s( V ) << log::line();
+}
+
+template <typename T>
+void print_bits( const void* value )
+{
+    size_t size = sizeof( T ) * 8;
+    T as_t      = *( (T*)value );
+    for ( int i = size - 1; i >= 0; i-- )
+    {
+        log::main << s( ( as_t >> i ) & 0x01 );
+    }
+    log::main << log::line();
+}
+
+void foo( char a, char b )
+{
+    log::main << "foo: " << s( a ) << ", " << s( b ) << log::line();
+}
+
+void bar( char a )
+{
+    log::main << "bar: " << s( a ) << log::line();
+}
+
+void foobar( void )
+{
+    log::main << "!";
+}
 
 void main( int argc, char* argv[] )
 {
-    log::source output( "engine.hello", log::write_to_file::immediately );
-    output.printf( "Hello \1 and \2 and \3 and \4 and \1", { "World", "friends", "rajin", "world2" }, "default" );
-    output.printf( "Hello \1", { "World!" }, "bad channel" );
-    output.printf( "Hello \1", { "World!" }, -1 );
+    char data[]    = { 0x04, 0x01, 0x00, 0x01, 0x00, 0x00, 0x00, 0x02 };
+    auto foo_call  = script::function_call<void, char, char>( foo );
+    auto foo_call2 = script::function_call( foo );
 
-    output << log::start() << "This is a message. There are (" << s( argc ) << ") args." << log::line();
+    foo_call.invoke( data );
+    foo_call2.invoke( data );
+
+    auto bar_call = script::function_call( bar );
+    bar_call.invoke( data );
+
+    auto foobar_call = script::function_call( foobar );
+    foobar_call.invoke( data );
 }
