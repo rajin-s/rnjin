51558b165eb6c3ca136ab6ba44b1d401fab882fa
Rajin <rajin@rajinshankar.com>
2019-07-01 18:51:36 -0400

    Added ostream support for vector types, and template min/max

---
+++ source/module/core/private/math.cpp
@@ -0,0 +1,15 @@
+/* *** ** *** ** *** ** *** *
+ * Part of rnjin            *
+ * (c) Rajin Shankar, 2019  *
+ *        rajinshankar.com  *
+ * *** ** *** ** *** ** *** */
+
+#include "math.hpp"
+
+namespace rnjin
+{
+    namespace core
+    {
+        
+    }
+}
--- source/module/core/public/math.hpp
+++ source/module/core/public/math.hpp
@@ -6,6 +6,8 @@
 
 #pragma once
 
+#include <ostream>
+
 namespace rnjin
 {
     namespace core
@@ -90,6 +92,22 @@ namespace rnjin
             }
         };
 
+        template <typename T>
+        std::ostream& operator<<( std::ostream& stream, vector2<T> v )
+        {
+            stream << "(" << v.x << ", " << v.y << ")";
+        }
+        template <typename T>
+        std::ostream& operator<<( std::ostream& stream, vector3<T> v )
+        {
+            stream << "(" << v.x << ", " << v.y << ", " << v.z << ")";
+        }
+        template <typename T>
+        std::ostream& operator<<( std::ostream& stream, vector4<T> v )
+        {
+            stream << "(" << v.x << ", " << v.y << ", " << v.z << ", " << v.w << ")";
+        }
+
         using float2 = vector2<float>;
         using float3 = vector3<float>;
         using float4 = vector4<float>;
@@ -101,6 +119,65 @@ namespace rnjin
         using int2 = vector2<int>;
         using int3 = vector3<int>;
         using int4 = vector4<int>;
+
+        // Template math functions
+        
+        // Max of an arbitrary number of comparable items
+        template <typename T, typename... Ts>
+        T max( T first, T second, Ts... rest )
+        {
+            if (first >= second)
+            {
+                return max(first, rest...);
+            }
+            else
+            {
+                return max(second, rest...);
+            }
+        }
+
+        template <typename T>
+        T max( T single )
+        {
+            return single;
+        }
+        
+        // Min of an arbitrary number of comparable items
+        template <typename T, typename... Ts>
+        T min( T first, T second, Ts... rest )
+        {
+            if (first <= second)
+            {
+                return min(first, rest...);
+            }
+            else
+            {
+                return min(second, rest...);
+            }
+        }
+
+        template <typename T>
+        T min( T single )
+        {
+            return single;
+        }
+
+        template <typename T>
+        T clamp(T value, T min, T max)
+        {
+            if (value <= min)
+            {
+                return min;
+            }
+            else if (value >= max)
+            {
+                return max;
+            }
+            else
+            {
+                return value;
+            }
+        }
 
     } // namespace core
 } // namespace rnjin
