4678c13c2e0c3e04ac1ee73bd93eeed1592e8170
Rajin <rajin@rajinshankar.com>
2019-09-11 22:18:56 -0400

    Removed old test files

--- source/test/tests.generated.hpp
+++
@@ -1,6 +0,0 @@
-#include "unit_test.hpp"
-#include "unit_test/bitmask.test.hpp"
-#include "unit_test/file.test.hpp"
-#include "unit_test/log.test.hpp"
-#include "unit_test/math.test.hpp"
-#define TESTS GET_TEST(bitmask), GET_TEST(file), GET_TEST(log), GET_TEST(math)
