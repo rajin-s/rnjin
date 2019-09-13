6a904fe1d4b91091b169a3d084a203633c48138f
Rajin <rajin@rajinshankar.com>
2019-09-13 10:25:44 -0400

    Removed unused portion of macro include

--- source/module/core/public/macro.hpp
+++ source/module/core/public/macro.hpp
@@ -43,46 +43,6 @@
 #define not !
 /* clang-format on */
 
-// #define get_( func_name, var_name )    \
-//     const auto get_##func_name() const \
-//     {                                  \
-//         return var_name;               \
-//     }
-
-// #define getref_( func_name, var_name )  \
-//     const auto& get_##func_name() const \
-//     {                                   \
-//         return var_name;                \
-//     }
-
-// #define __lparen (
-// #define __rparen )
-
-// #define through ,
-// #define __make_getter(var, name) const auto name() const { return var; }
-// #define __make_ref_getter(var, name) const auto& name() const { return var; }
-// #define make_value_accessible(var_as_name) __make_getter __lparen var_as_name __rparen
-// #define make_reference_accessible(var_as_name) __make_ref_getter __lparen var_as_name __rparen
-
-// #define returns ,
-// #define __make_accessor(name, var) const auto name() const { return var; }
-// #define __make_ref_accessor(name, var) const auto& name() const { return var; }
-// #define accessor(name_returns_var) __make_accessor __lparen name_returns_var __rparen
-// #define ref_accessor(name_returns_var) __make_ref_accessor __lparen name_returns_var __rparen
-
-// #define accessor(method_name) const auto method_name() const
-// #define ref_accessor(method_name) const auto& method_name() const
-
-// #define get_(name) const auto get_##name() const
-// #define returns(return_value) { return return_value; }
-
-// #define accessor const auto
-// #define returns(value) () const { return value; }
-
-// #define access(value) () const { return value; }
-
-// #define wrap_value(value) () const { return value; }
-
 // simple accessor macro to use with let
 // example: let get_name get_value( name );
 //          let& get_name get_value( name );
@@ -99,8 +59,3 @@
 #define __raw_function_string __FUNCTION__
 #define __raw_file_string __FILE__
 #define __raw_line_string __STR( __LINE__ )
-
-// #define __file_path_separator '\\'
-
-// #define __raw_call_string __raw_file_string " @ <" __raw_function_string ":" __raw_line_string ">"
-// #define _get_call_string() ( strrchr( __raw_call_string, __file_path_separator ) ? strrchr( __raw_call_string, __file_path_separator ) + 1 : __raw_call_string )
