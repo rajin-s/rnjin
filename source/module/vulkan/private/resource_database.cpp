bd7d8c28d01fd2b2404481f62f5401cd5a03b1c1
Rajin <rajin@rajinshankar.com>
2019-09-14 12:13:39 -0400

    Started resource_database

--- source/module/vulkan/private/resource_database.cpp
+++ source/module/vulkan/private/resource_database.cpp
@@ -8,19 +8,20 @@
 
 namespace rnjin::graphics::vulkan
 {
-    resource_database::resource_database( renderer_internal& parent ) : parent( parent ) {}
+    resource_database::resource_database( renderer_internal& parent ) : child_class( parent ) {}
     resource_database::~resource_database() {}
 
-    const resource_database::meshes::entry resource_database::meshes::get( const resource::id resource_id ) const
+    const resource_database::meshes::entry& resource_database::meshes::get( const resource::id resource_id ) const
     {
-        static const resource_database::meshes::entry invalid_entry( vk::Buffer(), vk::DeviceMemory() );
+        let static invalid_entry = resource_database::meshes::entry( vk::Buffer(), vk::DeviceMemory() );
+
         check_error_condition( return invalid_entry, vulkan_log_errors, entries.count( resource_id ) == 0, "Failed to find Vulkan mesh resource with id=\1", resource_id );
         return entries.at( resource_id );
     }
 
-    const resource_database::meshes::entry resource_database::meshes::create( const mesh& mesh_resource )
-    {
+    // const resource_database::meshes::entry resource_database::meshes::create( const mesh& mesh_resource )
+    // {
         
-    }
+    // }
 
 } // namespace rnjin::graphics::vulkan
