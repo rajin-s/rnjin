e6860a80aada8d82eb54bff9e5fcaac81dc6cc48
Rajin <rajin@rajinshankar.com>
2019-10-29 22:13:59 -0400

    Added static event handler class

--- source/module/core/public/event.hpp
+++ source/module/core/public/event.hpp
@@ -93,6 +93,53 @@ namespace rnjin::core
         method_type method;
     };
 
+    template <typename... As>
+    class static_event_handler : public event_handler_base, public event_handler_args<As...>
+    {
+        private: // types
+        using event_type    = event<As...>;
+        using function_type = void( As... );
+
+        public: // methods
+        // Register this handler with the target event on creation
+        static_event_handler( event_type& target_event, function_type function ) : target_event( &target_event ), pass_member( function )
+        {
+            target_event.add_handler( this );
+        }
+        // Remove this handler from the target event if it still exists
+        ~static_event_handler()
+        {
+            if ( target_event != nullptr )
+            {
+                target_event->remove_handler( this );
+            }
+        }
+
+        // Call the handlers method on its instance
+        void invoke( As... args ) override
+        {
+            function( args... );
+        }
+
+        // Clear the target event pointer so it isn't improperly dereferenced during destruction
+        // note: called from the event's destructor (ie when a handler outlives its event)
+        void invalidate_event()
+        {
+            target_event = nullptr;
+        }
+
+        // Check if this handler is still associated with an event
+        // note: handlers without valid events can be freely deleted
+        bool has_valid_event()
+        {
+            return target_event != nullptr;
+        }
+
+        private: // members
+        event_type* target_event;
+        function_type function;
+    };
+
     // An event source (ie publisher, delegate, etc.)
     template <typename... As>
     class event
