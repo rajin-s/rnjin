020205edd3af0f92843617fdb25dc3470f18bcdf
Rajin <rajin@rajinshankar.com>
2019-12-07 22:59:04 -0500

    Fix cube primitive generation

--- source/module/graphics/private/primitives.cpp
+++ source/module/graphics/private/primitives.cpp
@@ -43,67 +43,67 @@ namespace rnjin::graphics::primitives
 
         let vertices = list<mesh::vertex>{
             // Left face
-            vertex( points[0], directions[1], float4(1, 1, 0, 1), uvs[1] ),
-            vertex( points[1], directions[1], float4(0, 1, 1, 1), uvs[0] ),
-            vertex( points[4], directions[1], float4(1, 0, 1, 1), uvs[3] ),
-            vertex( points[5], directions[1], float4(1, 1, 0, 1), uvs[2] ),
+            vertex( points[0], directions[1], float4(1, 0, 0, 1), uvs[0] ),
+            vertex( points[4], directions[1], float4(1, 0, 0, 1), uvs[2] ),
+            vertex( points[5], directions[1], float4(1, 0, 0, 1), uvs[3] ),
+            vertex( points[1], directions[1], float4(1, 0, 0, 1), uvs[1] ),
 
             // Back face
-            vertex( points[0], directions[5], float4(1, 1, 0, 1), uvs[1] ),
-            vertex( points[3], directions[5], float4(0, 1, 1, 1), uvs[0] ),
-            vertex( points[4], directions[5], float4(1, 0, 1, 1), uvs[3] ),
-            vertex( points[7], directions[5], float4(1, 1, 0, 1), uvs[2] ),
+            vertex( points[3], directions[5], float4(0, 1, 0, 1), uvs[0] ),
+            vertex( points[7], directions[5], float4(0, 1, 0, 1), uvs[2] ),
+            vertex( points[4], directions[5], float4(0, 1, 0, 1), uvs[3] ),
+            vertex( points[0], directions[5], float4(0, 1, 0, 1), uvs[1] ),
 
             // Top face
-            vertex( points[4], directions[2], float4(1, 1, 0, 1), uvs[1] ),
-            vertex( points[7], directions[2], float4(0, 1, 1, 1), uvs[0] ),
-            vertex( points[5], directions[2], float4(1, 0, 1, 1), uvs[3] ),
-            vertex( points[6], directions[2], float4(1, 1, 0, 1), uvs[2] ),
+            vertex( points[5], directions[2], float4(1, 1, 0, 1), uvs[0] ),
+            vertex( points[4], directions[2], float4(1, 1, 0, 1), uvs[2] ),
+            vertex( points[7], directions[2], float4(1, 1, 0, 1), uvs[3] ),
+            vertex( points[6], directions[2], float4(1, 1, 0, 1), uvs[1] ),
 
             // Front face
-            vertex( points[1], directions[4], float4(1, 1, 0, 1), uvs[1] ),
-            vertex( points[2], directions[4], float4(0, 1, 1, 1), uvs[0] ),
-            vertex( points[5], directions[4], float4(1, 0, 1, 1), uvs[3] ),
-            vertex( points[6], directions[4], float4(1, 1, 0, 1), uvs[2] ),
+            vertex( points[1], directions[4], float4(0, 0, 1, 1), uvs[0] ),
+            vertex( points[5], directions[4], float4(0, 0, 1, 1), uvs[2] ),
+            vertex( points[6], directions[4], float4(0, 0, 1, 1), uvs[3] ),
+            vertex( points[2], directions[4], float4(0, 0, 1, 1), uvs[1] ),
 
             // Right face
-            vertex( points[3], directions[0], float4(1, 1, 0, 1), uvs[1] ),
-            vertex( points[2], directions[0], float4(0, 1, 1, 1), uvs[0] ),
+            vertex( points[2], directions[0], float4(1, 0, 1, 1), uvs[0] ),
+            vertex( points[6], directions[0], float4(1, 0, 1, 1), uvs[2] ),
             vertex( points[7], directions[0], float4(1, 0, 1, 1), uvs[3] ),
-            vertex( points[6], directions[0], float4(1, 1, 0, 1), uvs[2] ),
+            vertex( points[3], directions[0], float4(1, 0, 1, 1), uvs[1] ),
 
             // Bottom face
-            vertex( points[3], directions[3], float4(1, 1, 0, 1), uvs[1] ),
             vertex( points[1], directions[3], float4(0, 1, 1, 1), uvs[0] ),
-            vertex( points[2], directions[3], float4(1, 0, 1, 1), uvs[3] ),
-            vertex( points[0], directions[3], float4(1, 1, 0, 1), uvs[2] ),
+            vertex( points[0], directions[3], float4(0, 1, 1, 1), uvs[2] ),
+            vertex( points[3], directions[3], float4(0, 1, 1, 1), uvs[3] ),
+            vertex( points[2], directions[3], float4(0, 1, 1, 1), uvs[1] ),
         };
 
         // clang-format off
         static let indices = list<mesh::index>{
             // Left face
             0, 1, 2,
-            1, 3, 2,
+            0, 2, 3,
 
             // Back face
             4, 5, 6,
-            5, 7, 6,
+            4, 6, 7,
             
             // Top face
             8, 9, 10,
-            9, 11, 10,
+            8, 10, 11,
             
             // Front face
             12, 13, 14,
-            13, 15, 14,
+            12, 14, 15,
             
             // Right face
             16, 17, 18,
-            17, 19, 18,
+            16, 18, 19,
             
             // Bottom face
-            20, 21, 22,
-            21, 23, 22,
+            22, 21, 20,
+            22, 20, 23,
         };
         // clang-format on
 
